6d958c155d17ebbf91ee640ee4d13465c5456b4c
Sahil <sahil@kokamkar.com>
2025-07-22 11:38:24 +0000

    feat: add benchmark

---
+++ benches/digipin_benches.rs
@@ -0,0 +1,19 @@
+use digipin::{get_digipin, get_coordinates_from_digipin};
+use divan::black_box;
+
+#[divan::bench]
+fn encode() {
+    let lat = black_box(28.6139f64);
+    let lon = black_box(77.2090f64);
+    get_digipin(lat, lon).unwrap();
+}
+
+#[divan::bench]
+fn decode() {
+    let pin = black_box("FCJ-3F9-8273");
+    get_coordinates_from_digipin(pin).unwrap();
+}
+
+fn main() {
+    divan::main();
+} 
